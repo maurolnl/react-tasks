4543b5f26cc78a80f3373eca49b56764a8964ddd
maurolnl <mauroleoquiroga@gmail.com>
2021-08-12 15:58:48 -0300

    Add footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer">
       <p className="footer-text">
         Made with ♡ by&nbsp;{""}
-        <a href="https:www.github.com/maurolnl" className="footer-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://maurolquiroga.site/" className="footer-link" target="_blank" rel="noopener noreferrer">
           MauroLNL
         </a>
       </p>
